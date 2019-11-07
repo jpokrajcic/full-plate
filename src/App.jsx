360d241fa76187ddc0b8c7c8a95c6e16c968a6d5
Your NameJosip <josip.pokrajcic@binaria.hr>
2019-11-07 19:28:35 +0100

    ESLint + Prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,5 +24,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
